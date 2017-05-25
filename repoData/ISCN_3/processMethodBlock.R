f3f1e91cd639736fb69462bb01055325e86e88d0
K Todd-Brown <ktoddbrown@gmail.com>
2017-05-25 11:10:58 -0700

    filter based on -999 NA value; add debugger; only take unique values
    Former-commit-id: f38a60c4537ef2878303bf63ab760b1f0b823590

--- repoData/ISCN_3/processMethodBlock.R
+++ repoData/ISCN_3/processMethodBlock.R
@@ -1,5 +1,18 @@
+#' Process a methods block
+#' 
+#' Force the samples to discard repeats
+#'
+#' @param methodNames 
+#' @param unitName 
+#' @param sampleTemp 
+#' @param unit.df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 processMethodBlock <- function(methodNames=NULL, unitName=NULL,
-                               sampleTemp, unit.df=NULL){
+                               sampleTemp, unit.df=NULL, verbose=FALSE){
   
   ##Merge methods
   sampleTemp$method <- ''
@@ -11,12 +24,12 @@ processMethodBlock <- function(methodNames=NULL, unitName=NULL,
     sampleTemp$method <- gsub('^\\s+', '', sampleTemp$method)
     sampleTemp[,methodStr] <- NULL
   }
-  
+  if(verbose) cat('flag1')
   #melt values
-  sampleTemp <- melt(sampleTemp, id.vars=c(c('fieldID', 'method'), unitName), 
+  sampleTemp <- unique(melt(sampleTemp, id.vars=c(c('fieldID', 'method'), unitName), 
                      variable.name='measurement', 
-                     na.rm=TRUE, forceNumericValue=TRUE)
-  
+                     na.rm=TRUE, forceNumericValue=TRUE))
+  if(verbose) cat('flag2')
   if(!is.null(unitName)){
     names(sampleTemp)[3] <- 'unit'
   }else if(is.null(unitName) & !is.null(unit.df)){
@@ -25,11 +38,12 @@ processMethodBlock <- function(methodNames=NULL, unitName=NULL,
   }else{
     warning('units not specified')
   }
-  
+  if(verbose) cat('flag3')
   #issolate unique measurements
   measurementTemp <- unique(sampleTemp[c('measurement','method')])
   measurementTemp$measurementID <- sprintf('%s_%02d', measurementTemp$measurement, 
                                            1:nrow(measurementTemp))
+  if(verbose) cat('flag4')
   sampleTemp <- merge(sampleTemp, measurementTemp)[, c('fieldID', 'measurementID',
                                                        'value', 'unit')]
   
@@ -37,7 +51,9 @@ processMethodBlock <- function(methodNames=NULL, unitName=NULL,
   names(measurementTemp)[1] <- 'type'
   
   sampleTemp$value <- as.numeric(sampleTemp$value)
-  
+  if(verbose) cat('flag5')
+  sampleTemp <- sampleTemp[sampleTemp$value != -999, ]
+  if(verbose) cat('flag6')
   return(list(sample=sampleTemp, measurement=measurementTemp))
   
 }
